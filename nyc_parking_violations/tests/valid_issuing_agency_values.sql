2a19345f4eec09bc1c815a2c676338403e12b5cb
Mark Freeman <mark@onthemarkdata.com>
2025-05-24 22:35:28 -0700

    setup base project

--- nyc_parking_violations/tests/valid_issuing_agency_values.sql
+++
@@ -1,8 +0,0 @@
-{{ config(severity = 'warn') }}
-
-SELECT 
-    issuing_agency
-FROM 
-    {{ ref('silver_valid_violation_tickets') }}
-WHERE 
-    issuing_agency IN ('XYZ')
--- nyc_parking_violations/tests/valid_violation_county_values.sql
+++
@@ -1,8 +0,0 @@
-{{ config(severity = 'warn') }}
-
-SELECT 
-    violation_county
-FROM 
-    {{ ref('silver_valid_violation_tickets') }}
-WHERE 
-    violation_county IN ('King''s', 'KINGS', 'Queens', 'QNS')
